2bcefe67def745ff2b1d3c9f8fcf7379b51cfd67
kvasbo <audun@kvasbo.no>
2019-08-16 12:58:35 +0200

    Kind of sort of working in a way

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import parseDnb from './parsers/parser.dnb';
+import ReactTable from 'react-table'
 import './App.css';
+import "react-table/react-table.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface YnabLine {
+  date: string;
+  payee: string;
+  memo: string;
+  inflow: number | null;
+  outflow: number | null;
+}
+
+interface AppState {
+  unparsed: string;
+  parsed: YnabLine[];
+}
+
+const columns = [{
+  Header: 'Date',
+  accessor: 'date' // String-based value accessors!
+}, {
+  Header: 'Payee',
+  accessor: 'payee',
+}, {
+  accessor: 'memo', // Required because our accessor is not a string
+  Header: 'Memo',
+}, {
+  accessor: 'inflow', 
+  Header: 'Inflow'
+}, {
+  accessor: 'outflow', 
+  Header: 'Outflow'
+}]
+
+class App extends React.PureComponent<{}, AppState> {
+  public constructor(props: {}) {
+    super(props);
+    this.state = { unparsed: "", parsed: [] } 
+  }
+
+  private parseInput = (data: string) => {
+    const p = parseDnb(data);
+    this.setState({ parsed: p });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <textarea onChange={(e: React.ChangeEvent<HTMLTextAreaElement>)=> {this.parseInput(e.currentTarget.value);}} />
+          </p>
+          <ReactTable
+            data={this.state.parsed}
+            columns={columns}
+            className="-striped -highlight"
+          />
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
