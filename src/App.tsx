edf792e9a024443033c086539ce4433407c271d6
kvasbo <audun@kvasbo.no>
2019-09-13 11:22:17 +0200

    Linting etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,84 @@
 import React from 'react';
 import parseDnb from './parsers/parser.dnb';
-import ReactTable from 'react-table'
+import ReactTable from 'react-table';
+import Dropzone from 'react-dropzone';
 import './App.css';
-import "react-table/react-table.css";
+import 'react-table/react-table.css';
 
 export interface YnabLine {
-  date: string;
-  payee: string;
-  memo: string;
-  inflow: number | null;
-  outflow: number | null;
+    date: string;
+    payee: string;
+    memo: string;
+    inflow: number | null;
+    outflow: number | null;
 }
 
 interface AppState {
-  unparsed: string;
-  parsed: YnabLine[];
+    unparsed: string;
+    parsed: YnabLine[];
 }
 
-const columns = [{
-  Header: 'Date',
-  accessor: 'date' // String-based value accessors!
-}, {
-  Header: 'Payee',
-  accessor: 'payee',
-}, {
-  accessor: 'memo', // Required because our accessor is not a string
-  Header: 'Memo',
-}, {
-  accessor: 'inflow', 
-  Header: 'Inflow'
-}, {
-  accessor: 'outflow', 
-  Header: 'Outflow'
-}]
+const columns = [
+    {
+        Header: 'Date',
+        accessor: 'date', // String-based value accessors!
+    },
+    {
+        Header: 'Payee',
+        accessor: 'payee',
+    },
+    {
+        accessor: 'memo', // Required because our accessor is not a string
+        Header: 'Memo',
+    },
+    {
+        accessor: 'inflow',
+        Header: 'Inflow',
+    },
+    {
+        accessor: 'outflow',
+        Header: 'Outflow',
+    },
+];
 
 function download(filename: string, data: YnabLine[]) {
-  const element = document.createElement('a');
-  const text="Test nummer to"
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
+    const element = document.createElement('a');
+    const text = 'Test nummer to';
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
 
-  element.style.display = 'none';
-  document.body.appendChild(element);
+    element.style.display = 'none';
+    document.body.appendChild(element);
 
-  element.click();
+    element.click();
 
-  document.body.removeChild(element);
+    document.body.removeChild(element);
 }
 
 class App extends React.PureComponent<{}, AppState> {
-  public constructor(props: {}) {
-    super(props);
-    this.state = { unparsed: "", parsed: [] } 
-  }
+    public constructor(props: {}) {
+        super(props);
+        this.state = { unparsed: '', parsed: [] };
+    }
 
-  private parseInput = (data: string) => {
-    const p = parseDnb(data);
-    this.setState({ parsed: p });
-  }
+    private parseInput = (data: string) => {
+        const p = parseDnb(data);
+        this.setState({ parsed: p });
+    };
 
-  render() {
-    return (
-      <div className="App">
-        <p>
-          <textarea onChange={(e: React.ChangeEvent<HTMLTextAreaElement>)=> {this.parseInput(e.currentTarget.value);}} />
-        </p>
-        <ReactTable
-          data={this.state.parsed}
-          columns={columns}
-          className="-striped -highlight"
-        />
-        <span onClick={() => download('ynab.csv', this.state.parsed)} >download</span>
-      </div>
-    );
-  }
-  
+    render() {
+        return (
+            <div className="App">
+                <textarea
+                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                        this.parseInput(e.currentTarget.value);
+                    }}
+                />
+                <ReactTable data={this.state.parsed} columns={columns} className="-striped -highlight" />
+                <span onClick={() => download('ynab.csv', this.state.parsed)}>download</span>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/parsers/parser.dnb.ts
+++ src/parsers/parser.dnb.ts
@@ -2,22 +2,22 @@ import Moment from 'moment';
 import papaparse from 'papaparse';
 import { YnabLine } from '../App';
 
-const parse = (data: string) : YnabLine[] => {
-  // Parse dnb kredittkort
-  const d = papaparse.parse(data, { dynamicTyping: false, transform: val => val.trim() });
-  const parsed = parseDnbKreditt(d.data);
-  console.log(d);
-  console.log(parsed);
-  return parsed;
+const parse = (data: string): YnabLine[] => {
+    // Parse dnb kredittkort
+    const d = papaparse.parse(data, { dynamicTyping: false, transform: val => val.trim() });
+    const parsed = parseDnbKreditt(d.data);
+    console.log(d);
+    console.log(parsed);
+    return parsed;
 };
 
 function parseNumber(value: string, locale = navigator.language) {
-  const example = Intl.NumberFormat(locale).format(1.1);
-  const cleanPattern = new RegExp(`[^-+0-9${ example.charAt( 1 ) }]`, 'g');
-  const cleaned = value.replace(cleanPattern, '');
-  const normalized = cleaned.replace(example.charAt(1), '.');
+    const example = Intl.NumberFormat(locale).format(1.1);
+    const cleanPattern = new RegExp(`[^-+0-9${example.charAt(1)}]`, 'g');
+    const cleaned = value.replace(cleanPattern, '');
+    const normalized = cleaned.replace(example.charAt(1), '.');
 
-  return parseFloat(normalized);
+    return parseFloat(normalized);
 }
 
 /*
@@ -30,21 +30,21 @@ function parseNumber(d: string): number | null {
 }*/
 
 function parseDnbKreditt(data: string[][]): YnabLine[] {
-  const out: YnabLine[] = [];
-  data.forEach((l, i) => {
-    //TODO Detect first line
-    const d: Moment.Moment = Moment(l[0], "DD.MM.YYYY");
-    const date: string = d.format("YYYY-MM-DD");
-    const payee: string = "";
-    const memo: string = l[2];
-    const inflow = l[5] ? parseNumber(l[5]) : null;
-    const outflow = l[6] ? parseNumber(l[6]) : null;
-    const line: YnabLine = { date, payee, memo, inflow, outflow }
-    out.push(line);
-  })
-  // console.log(out);
-  
-  return out;
+    const out: YnabLine[] = [];
+    data.forEach((l, i) => {
+        //TODO Detect first line
+        const d: Moment.Moment = Moment(l[0], 'DD.MM.YYYY');
+        const date: string = d.format('YYYY-MM-DD');
+        const payee = '';
+        const memo: string = l[2];
+        const inflow = l[5] ? parseNumber(l[5]) : null;
+        const outflow = l[6] ? parseNumber(l[6]) : null;
+        const line: YnabLine = { date, payee, memo, inflow, outflow };
+        out.push(line);
+    });
+    // console.log(out);
+
+    return out;
 }
 
 export default parse;
