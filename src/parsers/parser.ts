a25cb9e9e1c4ec16eaa1a2fa5796832b221f81da
kvasbo <audun@kvasbo.no>
2019-09-15 12:21:12 +0200

    Work on parsing data to auto-identify fields

--- src/parsers/parser.ts
+++ src/parsers/parser.ts
@@ -1,4 +1,5 @@
 import papaparse from 'papaparse';
+import Moment from 'moment';
 
 export interface ParsedData {
     headers: string[];
@@ -13,6 +14,19 @@ export function parseNumber(value: string, locale = navigator.language): number 
     const normalized = cleaned.replace(example.charAt(1), '.');
 
     return parseFloat(normalized);
+}
+
+export function parseNumber2(strgx: string) {
+    let strg = strgx || '';
+    let decimal = '.';
+    strg = strg.replace(/[^0-9$.,]/g, '');
+    if (strg.indexOf(',') > strg.indexOf('.')) decimal = ',';
+    if ((strg.match(new RegExp('\\' + decimal, 'g')) || []).length > 1) decimal = '';
+    if (decimal != '' && strg.length - strg.indexOf(decimal) - 1 == 3 && strg.indexOf('0' + decimal) !== 0)
+        decimal = '';
+    strg = strg.replace(new RegExp('[^0-9$' + decimal + ']', 'g'), '');
+    strg = strg.replace(',', '.');
+    return parseFloat(strg);
 }
 
 /**
@@ -36,6 +50,140 @@ function parseDnbKreditt(data: string[][]): YnabLine[] {
 
  */
 
+interface ColumnMetaData {
+    [s: number]: {
+        maxLength: number;
+        minLength: number;
+        allNumbers: boolean;
+        someDates: boolean;
+        allDates: boolean;
+        numbersCount: number;
+        numbersAverage: number;
+        probablyDate: number;
+        probablyMemo: number;
+        probablyInflow: number;
+        probablyOutflow: number;
+    };
+}
+
+function findFieldTypes(data: string[][]) {
+    const results: ColumnMetaData = [];
+    for (let i = 0; i < data.length; i++) {
+        results[i] = {
+            maxLength: 0,
+            minLength: 999,
+            allNumbers: true,
+            someDates: false,
+            allDates: true,
+            numbersCount: 0,
+            numbersAverage: 0,
+            // The main event!
+            probablyDate: 0,
+            probablyMemo: 0,
+            probablyInflow: 0,
+            probablyOutflow: 0,
+        };
+
+        data[i].forEach(d => {
+            if (d.length > results[i].maxLength) results[i].maxLength = d.length; // max length
+            if (d.length < results[i].minLength) results[i].minLength = d.length; // min length
+            // check if number
+            if (d.length > 12) {
+                results[i].allNumbers = false;
+            } // Too long, we don't support billions
+            // Do date parsing
+            if (d.length < 12 && d.length > 0) {
+                // significant variance in length
+                const date = Moment(d);
+                console.log(d, date);
+                const n = parseNumber2(d);
+                if (isNaN(n)) results[i].allNumbers = false;
+                else {
+                    results[i].numbersCount += 1;
+                }
+            }
+            const n = parseNumber2(d);
+            console.log(n);
+        });
+        // ANALYSIS HERE
+        // Significant variance in length
+        if (results[i].maxLength === results[i].minLength) {
+            results[i].probablyInflow += -100;
+            results[i].probablyOutflow += -100;
+            results[i].probablyMemo += -100;
+            results[i].probablyDate += 10;
+        }
+        if (results[i].maxLength - results[i].minLength > 4) {
+            results[i].allDates = false;
+            results[i].probablyDate += -1;
+            results[i].probablyMemo += 1;
+        } else {
+            results[i].probablyDate += 1;
+        }
+        // Even more significant variance in length
+        if (results[i].maxLength - results[i].minLength > 8) {
+            const lengthFactor = Math.max(0, results[i].maxLength - results[i].minLength - 5);
+            results[i].probablyInflow += -lengthFactor;
+            results[i].probablyOutflow += -lengthFactor;
+            results[i].probablyMemo += lengthFactor;
+            results[i].probablyDate += -lengthFactor;
+        }
+        if (results[i].allNumbers) {
+            results[i].probablyInflow += 2;
+            results[i].probablyOutflow += 2;
+            results[i].probablyMemo += -5;
+        }
+        if (results[i].minLength === 0) {
+            results[i].probablyInflow += 2;
+            results[i].probablyOutflow += 2;
+            results[i].probablyMemo += -2;
+            results[i].probablyDate += -2;
+        }
+        // More than half the lines are sums, probably outflow
+        if (results[i].allNumbers && results[i].numbersCount > data.length / 2) {
+            results[i].probablyOutflow += 5;
+            results[i].probablyMemo += -5;
+            // results[i].probablyDate += -5;
+        }
+        // More than 66% the lines are sums, probably outflow
+        if (results[i].allNumbers && results[i].numbersCount > data.length / 1.5) {
+            results[i].probablyOutflow += 5;
+            results[i].probablyMemo += -5;
+            // results[i].probablyDate += -5;
+        }
+        // Just a few, probably inflow!
+        if (results[i].allNumbers && results[i].numbersCount < data.length / 2) {
+            results[i].probablyInflow += 5;
+            results[i].probablyMemo += -5;
+            // results[i].probablyDate += -5;
+        }
+    }
+    console.log(results);
+}
+
+/**
+ * Try to infer data types
+ * @param data
+ * @param ignoreN Skip N rows from start
+ */
+function findFields(data: ParsedData['data'], ignoreN = 1): void | number {
+    // Get length
+    const length = data.length;
+    if (length < ignoreN + 1) return -1;
+    const width = data[ignoreN].length;
+    const vrengt: string[][] = [];
+    console.log(`Length: ${length}, Width: ${width}`);
+    // Invert array axes
+    for (let i = ignoreN; i < length; i++) {
+        for (let j = 0; j < width; j++) {
+            if (!vrengt[j]) vrengt[j] = []; // Init array
+            vrengt[j].push(data[i][j]);
+        }
+    }
+    console.log(vrengt);
+    findFieldTypes(vrengt);
+}
+
 const parse = (data: string): ParsedData => {
     // Parse dnb kredittkort
     const parsed: ParsedData = { headers: [], data: [] };
@@ -54,6 +202,8 @@ const parse = (data: string): ParsedData => {
         if (f.length !== headers.length) return false;
         return true;
     });
+
+    findFields(parsed.data);
 
     return parsed;
 };
