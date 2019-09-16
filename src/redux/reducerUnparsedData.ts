47aa787dc12ef02138203e5f85859a294928e4aa
kvasbo <audun@kvasbo.no>
2019-09-16 20:03:28 +0200

    More cleanup

--- src/redux/reducerUnparsedData.ts
+++ src/redux/reducerUnparsedData.ts
@@ -3,18 +3,18 @@ import { ADD_UNPARSED_DATA, UnparsedDataAction } from './actions';
 export type UnparsedData = string;
 
 export interface UnparsedDataState {
-    data: string[];
+    data?: string;
 }
 
-const intialState = { data: [] };
+const initialState = { data: undefined };
 
 export default function unparsedDataStore(
-    state: UnparsedDataState = intialState,
+    state: UnparsedDataState = initialState,
     action: UnparsedDataAction,
 ): UnparsedDataState {
     switch (action.type) {
         case ADD_UNPARSED_DATA: {
-            return { data: [action.data, ...state.data] };
+            return { data: action.data };
         }
         default:
             return state;
