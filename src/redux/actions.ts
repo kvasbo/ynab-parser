1bd9436aceeeb8a8ddd3ce9b8b95e934c5c8f574
kvasbo <audun@kvasbo.no>
2019-09-19 13:31:58 +0200

    Introducing redux for parser settings

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,10 +1,13 @@
 import { ErrorMessage } from './reducerErrorMessage';
 import { UnparsedData } from './reducerUnparsedData';
 import { ParsedData } from '../parsers/parser';
+import { ParserMapping, ParserSettings, parserSetting, parserMappingType } from './reducerParsedData';
 
 export const ADD_ERROR = 'ADD_ERROR';
 export const ADD_UNPARSED_DATA = 'ADD_UNPARSED_DATA';
 export const ADD_PARSED_DATA = 'ADD_PARSED_DATA';
+export const CHANGE_PARSER_SETTING = 'CHANGE_PARSER_SETTING';
+export const CHANGE_PARSER_MAPPING = 'CHANGE_PARSER_MAPPING';
 
 export interface ErrorMessageAction {
     type: typeof ADD_ERROR;
@@ -19,6 +22,23 @@ export interface UnparsedDataAction {
 export interface ParsedDataAction {
     type: typeof ADD_PARSED_DATA;
     data: ParsedData;
+}
+
+export interface ParsedDataAction {
+    type: typeof ADD_PARSED_DATA;
+    data: ParsedData;
+}
+
+export interface ParserSettingAction {
+    type: typeof CHANGE_PARSER_SETTING;
+    key: parserSetting;
+    value: boolean | string | number;
+}
+
+export interface ParserMappingAction {
+    type: typeof CHANGE_PARSER_MAPPING;
+    key: parserMappingType;
+    value: number;
 }
 
 export function addError(error: ErrorMessage): ErrorMessageAction {
@@ -39,5 +59,21 @@ export function addParsedData(data: ParsedData): ParsedDataAction {
     return {
         type: ADD_PARSED_DATA,
         data,
+    };
+}
+
+export function changeParserSetting(key: parserSetting, value: boolean | number): ParserSettingAction {
+    return {
+        type: CHANGE_PARSER_SETTING,
+        key,
+        value,
+    };
+}
+
+export function changeParserMapping(key: parserMappingType, value: number): ParserMappingAction {
+    return {
+        type: CHANGE_PARSER_MAPPING,
+        key,
+        value,
     };
 }
--- src/redux/reducerParsedData.ts
+++ src/redux/reducerParsedData.ts
@@ -3,14 +3,49 @@ import { ParsedData } from '../parsers/parser';
 
 export interface ParsedDataState {
     data?: ParsedData;
+    settings: ParserSettings;
+    mapping: ParserMapping;
 }
 
-const initialState = { data: undefined };
+export interface ParserSettings {
+    skipLines: number;
+    useHeader: boolean;
+    singleSumColumn: boolean;
+}
+
+export type parserSetting = 'skipLines' | 'useHeader' | 'singleSumColumn';
+
+const initSettings: ParserSettings = {
+    skipLines: 1,
+    useHeader: true,
+    singleSumColumn: false,
+};
+export interface ParserMapping {
+    date: number;
+    payee: number;
+    inflow: number;
+    outflow: number;
+    memo: number;
+    sum: number;
+}
+
+const initMapping: ParserMapping = {
+    date: -1,
+    payee: -1,
+    inflow: -1,
+    outflow: -1,
+    memo: -1,
+    sum: -1,
+};
+
+export type parserMappingType = 'date' | 'payee' | 'inflow' | 'outflow' | 'memo' | 'sum';
+
+const initialState = { data: undefined, mapping: initMapping, settings: initSettings };
 
 export default function parsedData(state: ParsedDataState = initialState, action: ParsedDataAction): ParsedDataState {
     switch (action.type) {
         case ADD_PARSED_DATA: {
-            return { data: action.data };
+            return { ...state, data: action.data };
         }
         default:
             return state;
