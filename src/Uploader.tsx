650fc126fccd6ef09177d874e1325c7ac880fb6b
Audun Kvasbø <audun@kvasbo.no>
2020-01-09 09:43:16 +0100

    WIP: Uploader
    http://jsfiddle.net/jimschubert/DqcUg/

--- src/Uploader.tsx
+++ src/Uploader.tsx
@@ -8,45 +8,58 @@ interface Props {
     isActive?: boolean;
 }
 
-function DropZone(props: Props): JSX.Element {
-    const onFiles = (acceptedFiles: File[]): void => {
+class DropZone extends React.PureComponent<Props, {}> {
+    ref: React.RefObject<unknown>;
+
+    constructor(props: Props) {
+        super(props);
+        this.ref = React.createRef();
+    }
+
+    onFiles = (acceptedFiles: File[]): void => {
         const reader = new FileReader();
-        reader.onabort = (): void => props.dispatch(addError({ category: 'error', message: 'File load aborted' }));
+        reader.onabort = (): void => this.props.dispatch(addError({ category: 'error', message: 'File load aborted' }));
         reader.onerror = (): void =>
-            props.dispatch(addError({ category: 'error', message: 'File reading has failed' }));
+            this.props.dispatch(addError({ category: 'error', message: 'File reading has failed' }));
         reader.onload = (): void => {
             // Do whatever you want with the file contents
             const binaryStr = reader.result;
             if (typeof binaryStr === 'string') {
-                props.dispatch(addUnparsedData(binaryStr));
+                this.props.dispatch(addUnparsedData(binaryStr));
             }
         };
         acceptedFiles.forEach((file: File): void => reader.readAsBinaryString(file));
     };
 
-    return (
-        <Dropzone onDrop={(acceptedFiles): void => onFiles(acceptedFiles)}>
-            {({ getRootProps, getInputProps }): JSX.Element => (
-                <section>
-                    <div
-                        style={{
-                            height: '15vh',
-                            width: '80vw',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            backgroundColor: 'rgb(240,240,240)',
-                            borderRadius: '1vw',
-                        }}
-                        {...getRootProps()}
-                    >
-                        <input {...getInputProps()} />
-                        <p>Drag and drop some files here, click to select a file, or paste data below.</p>
-                    </div>
-                </section>
-            )}
-        </Dropzone>
-    );
+    render(): JSX.Element {
+        return (
+            <div>
+                <textarea id={'test'}></textarea>
+
+                <Dropzone onDrop={(acceptedFiles): void => this.onFiles(acceptedFiles)}>
+                    {({ getRootProps, getInputProps }): JSX.Element => (
+                        <section>
+                            <div
+                                style={{
+                                    height: '15vh',
+                                    width: '80vw',
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    backgroundColor: 'rgb(240,240,240)',
+                                    borderRadius: '1vw',
+                                }}
+                                {...getRootProps()}
+                            >
+                                <input {...getInputProps()} />
+                                <p>Drag and drop some files here, click to select a file, or paste data below.</p>
+                            </div>
+                        </section>
+                    )}
+                </Dropzone>
+            </div>
+        );
+    }
 }
 
 export default connect()(DropZone);
