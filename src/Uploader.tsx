80b1fae36114b94e9017da7180dee0a463fa1c8d
Audun Kvasbø <audun@kvasbo.no>
2020-01-09 12:04:14 +0100

    WIP: Create our own dropzone

--- src/Uploader.tsx
+++ src/Uploader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Dropzone from 'react-dropzone';
+// import Dropzone from 'react-dropzone';
 import { addUnparsedData, addError } from './redux/actions';
 
 interface Props {
@@ -9,13 +9,14 @@ interface Props {
 }
 
 class DropZone extends React.PureComponent<Props, {}> {
-    ref: React.RefObject<unknown>;
+    textArea: React.RefObject<HTMLTextAreaElement>;
 
     constructor(props: Props) {
         super(props);
-        this.ref = React.createRef();
+        this.textArea = React.createRef();
     }
 
+    /*
     onFiles = (acceptedFiles: File[]): void => {
         const reader = new FileReader();
         reader.onabort = (): void => this.props.dispatch(addError({ category: 'error', message: 'File load aborted' }));
@@ -30,11 +31,66 @@ class DropZone extends React.PureComponent<Props, {}> {
         };
         acceptedFiles.forEach((file: File): void => reader.readAsBinaryString(file));
     };
+    */
+
+    onDragOver = (e: React.DragEvent<HTMLTextAreaElement>): void => {
+        console.log('Drag over');
+    };
+
+    onDragEnd = (e: React.DragEvent<HTMLTextAreaElement>): void => {
+        console.log('Drag end');
+    };
+
+    onDrop = (e: React.DragEvent<HTMLTextAreaElement>): void => {
+        e.preventDefault();
+        if (typeof window.FileReader === 'undefined') {
+            alert('File upload not supported');
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = (): void => {
+            const binaryStr = reader.result;
+            if (typeof binaryStr === 'string') {
+                this.props.dispatch(addUnparsedData(binaryStr));
+                // console.log(binaryStr);
+            } else {
+                alert('Could not read file as text');
+            }
+        };
+        const file = e.dataTransfer.files[0];
+        reader.readAsText(file);
+    };
+
+    onTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+        const newValue = e.target.value;
+        console.log(newValue);
+    };
 
     render(): JSX.Element {
+        //  No file reader, give up
+        if (typeof window.FileReader === 'undefined') {
+            return <div>File upload not supported</div>;
+        }
+
         return (
-            <div>
-                <textarea id={'test'}></textarea>
+            <div style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
+                <textarea
+                    style={{ display: 'flex', flex: 1, borderRadius: 5, borderColor: '#AAAAAA' }}
+                    ref={this.textArea}
+                    placeholder="Drag and drop some files here, click to select a file, or paste data below."
+                    onChange={this.onTextChange}
+                    onDragOver={this.onDragOver}
+                    onDragEnd={this.onDragEnd}
+                    onDrop={this.onDrop}
+                ></textarea>
+            </div>
+        );
+    }
+}
+
+export default connect()(DropZone);
+
+/*
 
                 <Dropzone onDrop={(acceptedFiles): void => this.onFiles(acceptedFiles)}>
                     {({ getRootProps, getInputProps }): JSX.Element => (
@@ -57,9 +113,5 @@ class DropZone extends React.PureComponent<Props, {}> {
                         </section>
                     )}
                 </Dropzone>
-            </div>
-        );
-    }
-}
 
-export default connect()(DropZone);
+                */
